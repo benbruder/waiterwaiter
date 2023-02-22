b45bb709a181a4d38d642baa279c88d973accb18
Ben Bruder <bbruder@mail.yu.edu>
2023-02-22 17:52:44 -0500

    Added fonts and various packages

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import {useCallback} from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter': require('./assets/fonts/Inter-Variable.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'Inter-Italic': require('./assets/fonts/Inter-Italic.ttf'),
+    'Alef': require('./assets/fonts/Alef-Regular.ttf'),
+    'Alef-Bold': require('./assets/fonts/Alef-Bold.ttf')
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World! (and waitresses)</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: 'Inter-Italic', fontSize: 40}}>WaiterWaiter</Text>
+      <Text style={{ fontFamily: 'Alef', fontSize: 20 }}>Hello World! (and waitresses)</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
 });
