92be56511ed394e4bf23fdf8dafe0e78bde9ebbb
Ben Bruder <bbruder@mail.yu.edu>
2023-02-26 14:16:35 -0500

    Created basic interface for table selection

--- App.js
+++ App.js
@@ -1,10 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import {useCallback} from "react";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 SplashScreen.preventAutoHideAsync();
+const Stack = createNativeStackNavigator();
+
+const TableScreen = ({navigation}) => {
+  return (
+      <View style={styles.scroll}>
+      <ScrollView>
+          <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '1'})}>
+                  <Text style={styles.table_text}>Table 1</Text>
+                  <Text style={styles.table_subtext}>(3 people)</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '2'})}>
+                  <Text style={styles.table_text}>Table 2</Text>
+                  <Text style={styles.table_subtext}>(5 people)</Text>
+              </TouchableOpacity>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '3'})}>
+                  <Text style={styles.table_text}>Table 3</Text>
+                  <Text style={styles.table_subtext}>(2 people)</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '4'})}>
+                  <Text style={styles.table_text}>Table 4</Text>
+                  <Text style={styles.table_subtext}>(8 people)</Text>
+              </TouchableOpacity>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '5'})}>
+                  <Text style={styles.table_text}>Table 5</Text>
+                  <Text style={styles.table_subtext}>(2 people)</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '6'})}>
+                  <Text style={styles.table_text}>Table 6</Text>
+                  <Text style={styles.table_subtext}>(8 people)</Text>
+              </TouchableOpacity>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '7'})}>
+                  <Text style={styles.table_text}>Table 7</Text>
+                  <Text style={styles.table_subtext}>(2 people)</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '8'})}>
+                  <Text style={styles.table_text}>Table 8</Text>
+                  <Text style={styles.table_subtext}>(8 people)</Text>
+              </TouchableOpacity>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity style={[styles.tables, {backgroundColor: '#ddd'}]} onPress={() => navigation.navigate('Add Table')}>
+                  <Text style={styles.table_add}>+</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.tables, {backgroundColor: 'rgba(0)'}]} />
+          </View>
+      </ScrollView>
+      </View>
+  );
+};
+
+
+
+const MenuScreen = ({navigation, route}) => {
+  return (
+      <View style={{flex: 10}}>
+      <Text>Table {route.params.name} Menu:</Text>
+      <Button
+          title="Broccoli"
+          onPress={() => navigation.navigate('Items', {name: 'Broccoli'})}
+      />
+      </View>
+  )
+};
+
+const ItemScreen = ({route}) => {
+  return (
+      <View style={{flex: 10}}>
+        <Text>Details for {route.params.name}:</Text>
+      </View>
+  )
+};
+
+const AddTableScreen = ({route}) => {
+    return (
+        <View style={{flex: 10}}>
+            <Text>Details for {route.params.name}:</Text>
+        </View>
+    )
+};
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,19 +113,50 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={{ fontFamily: 'Inter-Italic', fontSize: 40}}>WaiterWaiter</Text>
-      <Text style={{ fontFamily: 'Alef', fontSize: 20 }}>Hello World! (and waitresses)</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <View onLayout={onLayoutRootView}></View>
+        <Stack.Navigator initialRouteName="Tables">
+          <Stack.Screen name="Tables" options={{ title: "Select a Table" }} component={TableScreen} />
+          <Stack.Screen name="Menu" component={MenuScreen} />
+          <Stack.Screen name="Items" component={ItemScreen} />
+            <Stack.Screen name="Add Table" component={AddTableScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
+    scroll: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignContent: 'flex-start',
+        padding: 5
+    },
+    tables: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        aspectRatio: 1,
+        backgroundColor: '#3c3f41',
+        margin: 5,
+        borderRadius: 10
+    },
+    table_text: {
+        color: '#fff',
+        fontSize: 32,
+        fontFamily: 'Inter-Bold',
+    },
+    table_subtext: {
+        color: '#afb1b3',
+        fontSize: 20,
+        fontFamily: 'Inter'
+    },
+    table_add: {
+        color: '#000',
+        fontSize: 80,
+
+    }
 });
