dc314c0a16f3e96aaef45d89be1f3b2cc72e3f8e
Ben Bruder <bbruder@mail.yu.edu>
2023-03-15 13:36:42 -0400

    Revamped styles and data transfer Added working Figma icons

--- App.js
+++ App.js
@@ -9,78 +9,87 @@ SplashScreen.preventAutoHideAsync();
 const Stack = createNativeStackNavigator();
 
 const TableScreen = ({navigation}) => {
-  return (
-      <View style={styles.scroll}>
-      <ScrollView>
-          <View style={{flexDirection: 'row'}}>
-              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '1'})}>
-                  <Text style={styles.table_text}>Table 1</Text>
-                  <Text style={styles.table_subtext}>(3 people)</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '2'})}>
-                  <Text style={styles.table_text}>Table 2</Text>
-                  <Text style={styles.table_subtext}>(5 people)</Text>
-              </TouchableOpacity>
-          </View>
-          <View style={{flexDirection: 'row'}}>
-              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '3'})}>
-                  <Text style={styles.table_text}>Table 3</Text>
-                  <Text style={styles.table_subtext}>(2 people)</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '4'})}>
-                  <Text style={styles.table_text}>Table 4</Text>
-                  <Text style={styles.table_subtext}>(8 people)</Text>
-              </TouchableOpacity>
-          </View>
-          <View style={{flexDirection: 'row'}}>
-              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '5'})}>
-                  <Text style={styles.table_text}>Table 5</Text>
-                  <Text style={styles.table_subtext}>(2 people)</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '6'})}>
-                  <Text style={styles.table_text}>Table 6</Text>
-                  <Text style={styles.table_subtext}>(8 people)</Text>
-              </TouchableOpacity>
-          </View>
-          <View style={{flexDirection: 'row'}}>
-              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '7'})}>
-                  <Text style={styles.table_text}>Table 7</Text>
-                  <Text style={styles.table_subtext}>(2 people)</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {name: '8'})}>
-                  <Text style={styles.table_text}>Table 8</Text>
-                  <Text style={styles.table_subtext}>(8 people)</Text>
-              </TouchableOpacity>
-          </View>
-          <View style={{flexDirection: 'row'}}>
-              <TouchableOpacity style={[styles.tables, {backgroundColor: '#ddd'}]} onPress={() => navigation.navigate('Add Table')}>
-                  <Text style={styles.table_add}>+</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={[styles.tables, {backgroundColor: 'rgba(0)'}]} />
-          </View>
-      </ScrollView>
+   /**
+   * TODO:
+   *   - De-hard-coding table generation, table_id's
+   *   - Add functionality to 'add table' button
+   */
+   return (
+   <ScrollView><View style={[styles.scroll, {padding: 5}]}>
+      <View style={{flexDirection: 'row'}}>
+         <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {table_id: 1})}>
+            <Text style={styles.table_text}>Table 1</Text>
+            <Text style={styles.table_subtext}>(3 people)</Text>
+         </TouchableOpacity>
+         <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {table_id: 2})}>
+            <Text style={styles.table_text}>Table 2</Text>
+            <Text style={styles.table_subtext}>(5 people)</Text>
+         </TouchableOpacity>
       </View>
-  );
-};
-
-
-
-const MenuScreen = ({navigation, route}) => {
-  return (
-      <View style={{flex: 10}}>
-      <Text>Table {route.params.name} Menu:</Text>
-      <Button
-          title="Broccoli"
-          onPress={() => navigation.navigate('Items', {name: 'Broccoli'})}
-      />
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {table_id: 3})}>
+            <Text style={styles.table_text}>Table 3</Text>
+            <Text style={styles.table_subtext}>(2 people)</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {table_id: 4})}>
+            <Text style={styles.table_text}>Table 4</Text>
+            <Text style={styles.table_subtext}>(8 people)</Text>
+        </TouchableOpacity>
       </View>
-  )
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {table_id: 5})}>
+            <Text style={styles.table_text}>Table 5</Text>
+            <Text style={styles.table_subtext}>(2 people)</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {table_id: 6})}>
+            <Text style={styles.table_text}>Table 6</Text>
+            <Text style={styles.table_subtext}>(8 people)</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {table_id: 7})}>
+            <Text style={styles.table_text}>Table 7</Text>
+            <Text style={styles.table_subtext}>(2 people)</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tables} onPress={() => navigation.navigate('Menu', {table_id: 8})}>
+            <Text style={styles.table_text}>Table 8</Text>
+            <Text style={styles.table_subtext}>(8 people)</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity style={[styles.tables, {backgroundColor: '#ddd'}]} onPress={() => navigation.navigate('Tables')}>
+            <Text style={styles.table_add}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.tables, {backgroundColor: 'rgba(0)'}]} />
+      </View>
+   </View></ScrollView>
+   );
+   };
+
+
+
+   const MenuScreen = ({navigation, route}) => {
+   return (
+     <ScrollView><View style={styles.scroll}>
+           <Text style={[styles.table_text, {color: '#000', justifyContent: 'center', flexDirection: 'row'}]}>Table {route.params.table_id} Menu:</Text>
+           <Button
+               title="Broccoli"
+               onPress={() => navigation.navigate('Items', {name: 'Broccoli'})}
+           />
+           <Button
+               title="Beef"
+               onPress={() => navigation.navigate('Items', {name: 'Beef'})}
+           />
+       </View>
+     </ScrollView>
+   )
 };
 
 const ItemScreen = ({route}) => {
   return (
       <View style={{flex: 10}}>
-        <Text>Details for {route.params.name}:</Text>
+        <Text style={[styles.table_text, {color: '#000', justifyContent: 'center', flexDirection: 'row'}]}>Details for {route.params.name}:</Text>
+          <Text style={[styles.table_subtext]}> Lorem ipsum delor set...</Text>
       </View>
   )
 };
@@ -112,14 +121,15 @@ export default function App() {
     return null;
   }
 
-  return (
+  // noinspection JSUnresolvedVariable - route.params.table_id does in fact exist (JetBrains, more like NoBrains)
+    return (
       <NavigationContainer>
           <View onLayout={onLayoutRootView}></View>
-        <Stack.Navigator initialRouteName="Tables">
+          <Stack.Navigator initialRouteName="Tables">
           <Stack.Screen name="Tables" options={{ title: "Select a Table" }} component={TableScreen} />
-          <Stack.Screen name="Menu" component={MenuScreen} />
+          <Stack.Screen name="Menu" component={MenuScreen} options={({ route }) => ({title: 'Table ' + route.params.table_id + ' Menu'})} />
           <Stack.Screen name="Items" component={ItemScreen} />
-            <Stack.Screen name="Add Table" component={AddTableScreen} />
+          <Stack.Screen name="Add Table" component={AddTableScreen} />
         </Stack.Navigator>
       </NavigationContainer>
   );
@@ -127,20 +137,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
     scroll: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignContent: 'flex-start',
-        padding: 5
+        flexGrow: 1,
+        flex: 1
     },
     tables: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
         aspectRatio: 1,
-        backgroundColor: '#3c3f41',
+        backgroundColor: '#f17a24',
         margin: 5,
         borderRadius: 10
     },
@@ -150,9 +155,9 @@ const styles = StyleSheet.create({
         fontFamily: 'Inter-Bold',
     },
     table_subtext: {
-        color: '#afb1b3',
+        color: '#000',
         fontSize: 20,
-        fontFamily: 'Inter'
+        fontFamily: 'Inter-Italic'
     },
     table_add: {
         color: '#000',
